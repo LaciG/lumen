1bb7cc9eb44da3a4c94d47b151ba35664d729b10
László Gábor <lacigabi@windowslive.com>
2018-02-16 13:16:29 +0100

    Web Routes

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,37 @@ $router->get('/hello/world', function () use ($router){
 $router->get('/hello/{name}', ['middleware' => 'hello', function ($name) {
     return "Hello {$name}";
 }]);
+
+//2-21: Using the request object
+$router->get('/request', function (Illuminate\Http\Request $request){
+    return "Hello " . $request->get('name', 'stranger');
+});
+
+//2-23: Using the Illuminate Response Object
+/* $router->get('/response', function (Illuminate\Http\Request $request){
+    return (new Illuminate\Http\Response('Hello stranger', 200))->header('Content-Type', 'text/plain');
+}); */
+
+//2-24: Responding with JSON
+/* $router->get('/response', function (Illuminate\Http\Request $request){
+    if($request->wantsJson()){
+        return response()->json(['greeting' => 'Hello Stranger']);
+    }
+    return (new Illuminate\Http\Response('Hello stranger', 200))->header('Content-Type', 'text/plain');
+}); */
+
+//2-26: Using the ResponseFactory
+$router->get('/response', function (Illuminate\Http\Request $request){
+    if ($request->wantsJson()) {
+        return response()->json(['greeting' => 'Hello Stranger']);
+    }
+    return response()->make('Hello Stranger', 200, ['Content-Type' => 'text/plain']);
+});
+
+/* $router->get('/response', function (Illuminate\Http\Request $request){
+    if ($request->wantsJson()) {
+        return response()->json(['greeting' => 'Hello Stranger']);
+    }
+    return response()->json(['greeting' => 'Hello Stranger']);
+    //return response()->make('Hello Stranger', 200, ['Content-Type' => 'text/plain']);
+}); */
